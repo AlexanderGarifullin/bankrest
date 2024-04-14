4db5a970167ac133d3b89f7cdcf8aec77915c31d
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 19:04:29 +0500

    feat: add OrganizationalLegalForm model

---
+++ src/main/java/hse/greendata/bankrest/models/OrganizationalLegalForm.java
@@ -0,0 +1,30 @@
+package hse.greendata.bankrest.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Organizational_legal_form")
+@Setter
+@Getter
+@NoArgsConstructor
+public class OrganizationalLegalForm {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 100, message = "Name should be between 1 and 100 characters")
+    private String name;
+
+    @OneToMany(mappedBy = "organizationalLegalForm")
+    private List<Client> clients;
+}
