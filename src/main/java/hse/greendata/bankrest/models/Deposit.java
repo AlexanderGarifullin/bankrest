f91d64ccd695109e2de4d9ade9d2cec2b7bac58f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 19:10:25 +0500

    feat: add Deposit model

---
+++ src/main/java/hse/greendata/bankrest/models/Deposit.java
@@ -0,0 +1,55 @@
+package hse.greendata.bankrest.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Deposit")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Deposit {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "client_id")
+    @NotBlank(message = "Client Id is required")
+    private Integer clientId;
+
+    @Column(name = "bank_id")
+    @NotBlank(message = "Bank Id is required")
+    private Integer bankId;
+
+    @Column(name = "opening_date")
+    @NotBlank(message = "Opening date is required")
+    // !!! Should make validator for minDate !!!
+    private Date openingDate;
+
+    @Column(name = "interest_rate")
+    @NotBlank(message = "Interest rate is required")
+    @Min(value = 1, message = "Interest rate should be greater than zero")
+    private Double interestRate;
+
+    @Column(name = "term_months")
+    @NotBlank(message = "Term months is required")
+    @Min(value = 1, message = "Term months should be greater than zero")
+    private Integer termMonths;
+
+    @ManyToOne
+    @NotBlank(message = "Bank is required")
+    @JoinColumn(name = "bank_id", referencedColumnName = "id")
+    private Bank bank;
+
+    @ManyToOne
+    @NotBlank(message = "Client is required")
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client client;
+}
