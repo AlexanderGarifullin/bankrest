106e422c9feb21cacbd50dd7949e29c20321f04f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:31:46 +0500

    feat: make ClientDTO

--- src/main/java/hse/greendata/bankrest/dto/ClientDTO.java
+++ src/main/java/hse/greendata/bankrest/dto/ClientDTO.java
@@ -1,4 +1,34 @@
 package hse.greendata.bankrest.dto;
 
+import hse.greendata.bankrest.models.OrganizationalLegalForm;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ClientDTO {
+    @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 255, message = "Name should be between 1 and 255 characters")
+    private String name;
+
+    @NotBlank(message = "Short name is required")
+    @Size(min = 1, max = 50, message = "Name should be between 1 and 50 characters")
+    private String shortName;
+
+    @NotBlank(message = "Address is required")
+    @Size(min = 1, max = 255, message = "Name should be between 1 and 255 characters")
+    @Pattern(regexp = "^(?:[А-ЯЁ][а-яё]+),\\s(?:[А-ЯЁ][а-яё]+),\\s\\d{6},\\s(?:ул\\.|просп\\.)\\s(?:[А-ЯЁ][а-яё]+(?:\\s[А-ЯЁа-яё][а-яё]+)*)?,\\sд\\.\\s(?:[1-9]\\d*|0?[1-9]\\d*)$",
+            message = "Address should be in this format: County, City, Index, Street, Home")
+    private String address;
+
+    @ManyToOne
+    @NotBlank(message = "Organizational legal form is required")
+    @JoinColumn(name = "organizational_legal_form_id", referencedColumnName = "id")
+    private OrganizationalLegalForm organizationalLegalForm;
 }
