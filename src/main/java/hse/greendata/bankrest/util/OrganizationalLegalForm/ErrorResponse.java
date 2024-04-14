60ee89b419b35a078be0fa1f37f9447e7b575f2a
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 21:08:04 +0500

    feat: add ErrorResponse class for custom errors

---
+++ src/main/java/hse/greendata/bankrest/util/OrganizationalLegalForm/ErrorResponse.java
@@ -0,0 +1,15 @@
+package hse.greendata.bankrest.util.OrganizationalLegalForm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private long timestamp;
+}
