19580c2f577b7561c92eafb565bd59a4113b229e
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:40:13 +0500

    feat: add convertToBank and convertToBankDTO to BankController

--- src/main/java/hse/greendata/bankrest/controllers/BankController.java
+++ src/main/java/hse/greendata/bankrest/controllers/BankController.java
@@ -1,4 +1,31 @@
 package hse.greendata.bankrest.controllers;
 
+import hse.greendata.bankrest.dto.BankDTO;
+import hse.greendata.bankrest.dto.OrganizationalLegalFormDTO;
+import hse.greendata.bankrest.models.Bank;
+import hse.greendata.bankrest.models.OrganizationalLegalForm;
+import hse.greendata.bankrest.services.BankService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank")
 public class BankController {
+    private final BankService bankService;
+    private final ModelMapper modelMapper;
+    @Autowired
+    public BankController(BankService bankService, ModelMapper modelMapper) {
+        this.bankService = bankService;
+        this.modelMapper = modelMapper;
+    }
+
+    private Bank convertToBank(BankDTO bankDTO) {
+        return modelMapper.map(bankDTO, Bank.class);
+    }
+
+    private BankDTO convertToBankDTO(Bank bank){
+        return modelMapper.map(bank, BankDTO.class);
+    }
 }
