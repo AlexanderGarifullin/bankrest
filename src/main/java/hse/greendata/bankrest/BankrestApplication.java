4618006aaeb9b9e7993b0a1898db2dbb1303382f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 15:59:02 +0500

    Initial commit

---
+++ src/main/java/hse/greendata/bankrest/BankrestApplication.java
@@ -0,0 +1,13 @@
+package hse.greendata.bankrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hse/greendata/bankrest/BankrestApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.greendata.bankrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
