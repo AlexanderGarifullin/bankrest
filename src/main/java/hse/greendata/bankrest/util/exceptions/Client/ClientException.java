0ae687021cfffd75792c20f0384173fa746e4a10
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:45:57 +0500

    feat: create ClientException

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Client/ClientException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Client;
+
+public class ClientException extends RuntimeException{
+    public ClientException(String message) {
+        super(message);
+    }
+}
