7b9f40de72ee04c06ff58478806174e52e44baf4
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:23:11 +0500

    feat: add findByName to BankRepository

--- src/main/java/hse/greendata/bankrest/repositories/BankRepository.java
+++ src/main/java/hse/greendata/bankrest/repositories/BankRepository.java
@@ -4,6 +4,9 @@ import hse.greendata.bankrest.models.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Integer> {
+    Optional<Bank> findByName(String name);
 }
