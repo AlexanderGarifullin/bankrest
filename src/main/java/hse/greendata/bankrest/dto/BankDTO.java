a5a55ef158cf5e1b652d435c34dff91df3fe4688
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:37:47 +0500

    feat: add fields to BankDTO

--- src/main/java/hse/greendata/bankrest/dto/BankDTO.java
+++ src/main/java/hse/greendata/bankrest/dto/BankDTO.java
@@ -1,4 +1,23 @@
 package hse.greendata.bankrest.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BankDTO {
+    @Column(name = "name")
+    @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 255, message = "Name should be between 1 and 255 characters")
+    private String name;
+
+    @Column(name = "bik")
+    @NotBlank(message = "BIK is required")
+    @Pattern(regexp = "^\\d{9}$",
+            message = "BIK must be a 9-digit number")
+    private String bik;
 }
