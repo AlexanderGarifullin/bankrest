06f51da8155a15439140153b099432f0b160a34a
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 21:04:41 +0500

    feat: add ModelMapper bean

--- src/main/java/hse/greendata/bankrest/BankrestApplication.java
+++ src/main/java/hse/greendata/bankrest/BankrestApplication.java
@@ -1,7 +1,9 @@
 package hse.greendata.bankrest;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankrestApplication {
@@ -10,4 +12,8 @@ public class BankrestApplication {
 		SpringApplication.run(BankrestApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
