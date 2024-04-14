80f94d208fa6625c5503310022965d6e96a15046
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:47:03 +0500

    feat: create exception for client was not updated

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Client/ClientNotUpdatedException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Client;
+
+public class ClientNotUpdatedException extends ClientException{
+    public ClientNotUpdatedException(String message) {
+        super(message);
+    }
+}
