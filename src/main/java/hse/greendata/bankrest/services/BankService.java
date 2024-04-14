d43a24292d4665bc16477b9e732d081d8f6c6ab4
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:14:35 +0500

    feat: add delete to BankService

--- src/main/java/hse/greendata/bankrest/services/BankService.java
+++ src/main/java/hse/greendata/bankrest/services/BankService.java
@@ -1,12 +1,15 @@
 package hse.greendata.bankrest.services;
 
 import hse.greendata.bankrest.models.Bank;
+import hse.greendata.bankrest.models.OrganizationalLegalForm;
 import hse.greendata.bankrest.repositories.BankRepository;
+import hse.greendata.bankrest.util.exceptions.OrganizationalLegalForm.OrganizationalLegalFormNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,5 +26,8 @@ public class BankService {
         return bankRepository.findAll();
     }
 
-
+    @Transactional
+    public void delete(int id) {
+        bankRepository.deleteById(id);
+    }
 }
