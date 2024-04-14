d76c1fff66a7da41ee800c16142cbe8f16e3cb83
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:21:30 +0500

    feat: add exception for bank was not created

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Bank/BankNotCreatedException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Bank;
+
+public class BankNotCreatedException extends BankException{
+    public BankNotCreatedException(String message) {
+        super(message);
+    }
+}
