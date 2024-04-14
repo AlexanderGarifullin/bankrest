da82896072858399089305079af1d11c5fc894d9
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:19:29 +0500

    feat: add BankException for Bank model

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Bank/BankException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Bank;
+
+public class BankException extends RuntimeException{
+    public BankException(String message) {
+        super(message);
+    }
+}
