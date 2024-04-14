60127907be8baee0c0374a12fc6dcd135e910881
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:50:17 +0500

    feat: add findOne by id to ClientService

--- src/main/java/hse/greendata/bankrest/services/ClientService.java
+++ src/main/java/hse/greendata/bankrest/services/ClientService.java
@@ -3,11 +3,14 @@ package hse.greendata.bankrest.services;
 import hse.greendata.bankrest.models.Bank;
 import hse.greendata.bankrest.models.Client;
 import hse.greendata.bankrest.repositories.ClientRepository;
+import hse.greendata.bankrest.util.exceptions.Bank.BankNotFoundException;
+import hse.greendata.bankrest.util.exceptions.Client.ClientNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,4 +26,11 @@ public class ClientService {
     public List<Client> findAll() {
         return clientRepository.findAll();
     }
+
+    public Client findOne(int id) {
+        Optional<Client> foundClient = clientRepository.findById(id);
+        return foundClient.orElseThrow(() ->
+                new ClientNotFoundException("Client with id " + id + " not found"));
+    }
+
 }
