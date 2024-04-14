8550c6faee34e75f34da6fa623100a841cb86bcb
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:46:45 +0500

    feat: create exception for client was not found

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Client/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Client;
+
+public class ClientNotFoundException extends ClientException{
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
