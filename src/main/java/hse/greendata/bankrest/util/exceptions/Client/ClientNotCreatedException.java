8f0c787afbb9ce91374f162c7afc1bc91724da96
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 03:46:29 +0500

    feat: create exception for client was not created

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Client/ClientNotCreatedException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Client;
+
+public class ClientNotCreatedException extends ClientException{
+    public ClientNotCreatedException(String message) {
+        super(message);
+    }
+}
