15ee2267e472ee4333cecaaffe8773b75da5081e
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-14 21:36:59 +0500

    feat: add base exception for OrganizationalLegalForm

---
+++ src/main/java/hse/greendata/bankrest/util/OrganizationalLegalForm/exception/OrganizationalLegalForm/OrganizationalLegalFormException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.OrganizationalLegalForm.exception.OrganizationalLegalForm;
+
+public class OrganizationalLegalFormException extends RuntimeException {
+    public OrganizationalLegalFormException(String message) {
+        super(message);
+    }
+}
