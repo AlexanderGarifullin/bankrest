6883f7b60cfdb0d6e276bfaf27847d19af25b3dd
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:21:51 +0500

    feat: add exception for bank was not found

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Bank/BankNotFoundException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Bank;
+
+public class BankNotFoundException extends BankException{
+    public BankNotFoundException(String message) {
+        super(message);
+    }
+}
