288aab0e6f7bb3d7f356f9db8da4f656b15743dd
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 02:13:30 +0500

    feat: add findAll to BankService

--- src/main/java/hse/greendata/bankrest/services/BankService.java
+++ src/main/java/hse/greendata/bankrest/services/BankService.java
@@ -1,9 +1,12 @@
 package hse.greendata.bankrest.services;
 
+import hse.greendata.bankrest.models.Bank;
 import hse.greendata.bankrest.repositories.BankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,4 +18,10 @@ public class BankService {
     public BankService(BankRepository bankRepository) {
         this.bankRepository = bankRepository;
     }
+
+    public List<Bank> findAll() {
+        return bankRepository.findAll();
+    }
+
+
 }
