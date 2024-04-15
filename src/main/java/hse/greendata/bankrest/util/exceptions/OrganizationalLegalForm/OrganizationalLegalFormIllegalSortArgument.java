9ef50e2077f5946d459e599da73b5cd2757097be
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 15:28:09 +0500

    feat: add exception for illegal sort argument in OrganizationalLegalFormController

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/OrganizationalLegalForm/OrganizationalLegalFormIllegalSortArgument.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.OrganizationalLegalForm;
+
+public class OrganizationalLegalFormIllegalSortArgument extends OrganizationalLegalFormException{
+    public OrganizationalLegalFormIllegalSortArgument(String message) {
+        super(message);
+    }
+}
