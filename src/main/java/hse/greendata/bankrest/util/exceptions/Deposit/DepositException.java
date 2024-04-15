c33cc504082bf8b382f9fdba2654c825bfff8cc0
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 05:47:11 +0500

    feat: make DepositException

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Deposit/DepositException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Deposit;
+
+public class DepositException extends RuntimeException{
+    public DepositException(String message) {
+        super(message);
+    }
+}
