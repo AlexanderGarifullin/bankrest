90f46f392d9d98aed0ca5d8c246bc3ea0947f18d
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 14:18:30 +0500

    test: test find one deposit when he not exists and throw exception in DepositService (50% cov)

--- src/test/java/hse/greendata/bankrest/services/DepositServiceTest.java
+++ src/test/java/hse/greendata/bankrest/services/DepositServiceTest.java
@@ -3,6 +3,8 @@ package hse.greendata.bankrest.services;
 import hse.greendata.bankrest.models.Client;
 import hse.greendata.bankrest.models.Deposit;
 import hse.greendata.bankrest.repositories.DepositRepository;
+import hse.greendata.bankrest.util.exceptions.Client.ClientNotFoundException;
+import hse.greendata.bankrest.util.exceptions.Deposit.DepositNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -44,6 +46,17 @@ class DepositServiceTest {
         assertEquals(deposit.getOpeningDate(), result.getOpeningDate());
         assertEquals(deposit.getInterestRate(), result.getInterestRate());
         assertEquals(deposit.getTermMonths(), result.getTermMonths());
+
+        verify(depositRepository).findById(1);
+    }
+
+    @Test
+    void testFindOne_WhenDepositNotExists_ThrowException() {
+        when(depositRepository.findById(1)).thenReturn(Optional.empty());
+
+        assertThrows(DepositNotFoundException.class, () -> {
+            depositService.findOne(1);
+        });
 
         verify(depositRepository).findById(1);
     }
