c1c016f065121c9bc1736e1a6175960da6d7e232
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 05:47:38 +0500

    feat: add exception for deposit was not created

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Deposit/DepositNotCreatedException.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Deposit;
+
+public class DepositNotCreatedException extends DepositException{
+    public DepositNotCreatedException(String message) {
+        super(message);
+    }
+}
