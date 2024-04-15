2e67a29b299c4167af35e123aae3ea8b9e8c4731
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 15:43:37 +0500

    feat: add sort param to findAll in Client Service and Repository

--- src/main/java/hse/greendata/bankrest/repositories/ClientRepository.java
+++ src/main/java/hse/greendata/bankrest/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package hse.greendata.bankrest.repositories;
 
 import hse.greendata.bankrest.models.Client;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,6 @@ public interface ClientRepository extends JpaRepository<Client, Integer> {
     Optional<Client> findByName(String name);
 
     Optional<Client> findByShortName(String shortName);
+
+    List<Client> findAll(Sort sort);
 }
