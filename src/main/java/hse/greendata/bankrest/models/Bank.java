fd22de137a4dc0295440a02a404cb281ff44c601
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 10:26:57 +0500

    test: add all args to Bank model constructor for tests

--- src/main/java/hse/greendata/bankrest/models/Bank.java
+++ src/main/java/hse/greendata/bankrest/models/Bank.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Bank {
     @Id
     @Column(name = "id")
