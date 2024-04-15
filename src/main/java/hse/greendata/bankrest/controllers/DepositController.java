e8cca5eabd31352a464ae0bec08773dd56a4c976
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 05:59:06 +0500

    feat: add getClients by id to DepositController

--- src/main/java/hse/greendata/bankrest/controllers/DepositController.java
+++ src/main/java/hse/greendata/bankrest/controllers/DepositController.java
@@ -1,5 +1,7 @@
 package hse.greendata.bankrest.controllers;
 
+import hse.greendata.bankrest.dto.ClientDTO;
+import hse.greendata.bankrest.dto.DepositDTO;
 import hse.greendata.bankrest.models.Client;
 import hse.greendata.bankrest.models.Deposit;
 import hse.greendata.bankrest.services.DepositService;
@@ -7,6 +9,7 @@ import hse.greendata.bankrest.util.exceptions.ErrorMessagesBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,14 @@ public class DepositController {
     @GetMapping("")
     public List<Deposit> getClients(){
         return depositService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public DepositDTO getClients(@PathVariable("id") int id){
+        return convertToDepositDTO(depositService.findOne(id));
+    }
+
+    private DepositDTO convertToDepositDTO(Deposit deposit){
+        return modelMapper.map(deposit, DepositDTO.class);
     }
 }
