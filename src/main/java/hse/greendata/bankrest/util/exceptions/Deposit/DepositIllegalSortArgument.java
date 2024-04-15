56e926a434929535021b6faa4f70a0bc79b73fdf
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 15:45:55 +0500

    feat: add exception for illegal sort argument in Deposit Controller

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Deposit/DepositIllegalSortArgument.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Deposit;
+
+public class DepositIllegalSortArgument extends DepositException{
+    public DepositIllegalSortArgument(String message) {
+        super(message);
+    }
+}
