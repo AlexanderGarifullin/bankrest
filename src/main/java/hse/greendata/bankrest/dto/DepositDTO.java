e9b3e06f498d0cd494eda59efdc595bc16bae1d4
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 07:31:46 +0500

    ref: remove unused items from Deposit model and DTO

--- src/main/java/hse/greendata/bankrest/dto/DepositDTO.java
+++ src/main/java/hse/greendata/bankrest/dto/DepositDTO.java
@@ -1,15 +1,23 @@
 package hse.greendata.bankrest.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
+@Setter
+@Getter
 public class DepositDTO {
+    @NotNull(message = "Client id is required")
+    private Integer clientId;
+
+    @NotNull(message = "Bank id is required")
+    private Integer bankId;
+
     @NotNull(message = "Opening date is required")
-    // !!! Should make validator for minDate !!!
-    private Date openingDate;
+    private LocalDate openingDate;
 
     @NotNull(message = "Interest rate is required")
     @Min(value = 1, message = "Interest rate should be greater than zero")
@@ -18,10 +26,4 @@ public class DepositDTO {
     @NotNull(message = "Term months is required")
     @Min(value = 1, message = "Term months should be greater than zero")
     private Integer termMonths;
-
-    @NotNull(message = "Bank id is required")
-    private Integer bankId;
-
-    @NotNull(message = "Client id is required")
-    private Integer clientId;
 }
--- src/main/java/hse/greendata/bankrest/models/Deposit.java
+++ src/main/java/hse/greendata/bankrest/models/Deposit.java
@@ -2,13 +2,12 @@ package hse.greendata.bankrest.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "Deposit")
@@ -21,10 +20,17 @@ public class Deposit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "client_id")
+    @NotNull(message = "Client id is required")
+    private Integer clientId;
+
+    @Column(name = "bank_id")
+    @NotNull(message = "Bank id is required")
+    private Integer bankId;
+
     @Column(name = "opening_date")
     @NotNull(message = "Opening date is required")
-    // !!! Should make validator for minDate !!!
-    private Date openingDate;
+    private LocalDate openingDate;
 
     @Column(name = "interest_rate")
     @NotNull(message = "Interest rate is required")
@@ -35,12 +41,4 @@ public class Deposit {
     @NotNull(message = "Term months is required")
     @Min(value = 1, message = "Term months should be greater than zero")
     private Integer termMonths;
-
-    @Column(name = "bank_id")
-    @NotNull(message = "Bank id is required")
-    private Integer bankId;
-
-    @Column(name = "client_id")
-    @NotNull(message = "Client id is required")
-    private Integer clientId;
 }
