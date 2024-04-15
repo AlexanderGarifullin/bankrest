7b0840941f390d9d48304e8d6152e2e77e2ab31c
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 15:44:10 +0500

    feat: add exception for illegal sort argument in ClientController

---
+++ src/main/java/hse/greendata/bankrest/util/exceptions/Client/ClientIllegalSortArgument.java
@@ -0,0 +1,7 @@
+package hse.greendata.bankrest.util.exceptions.Client;
+
+public class ClientIllegalSortArgument extends ClientException{
+    public ClientIllegalSortArgument(String message) {
+        super(message);
+    }
+}
