51e45375ecab85f49417ab4e4c755e39e7548d30
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-15 05:58:40 +0500

    feat: add findOne by id to DepositService

--- src/main/java/hse/greendata/bankrest/services/DepositService.java
+++ src/main/java/hse/greendata/bankrest/services/DepositService.java
@@ -3,11 +3,14 @@ package hse.greendata.bankrest.services;
 import hse.greendata.bankrest.models.Client;
 import hse.greendata.bankrest.models.Deposit;
 import hse.greendata.bankrest.repositories.DepositRepository;
+import hse.greendata.bankrest.util.exceptions.Client.ClientNotFoundException;
+import hse.greendata.bankrest.util.exceptions.Deposit.DepositNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,5 +26,9 @@ public class DepositService {
     public List<Deposit> findAll() {
         return depositRepository.findAll();
     }
-
+    public Deposit findOne(int id) {
+        Optional<Deposit> foundDeposit = depositRepository.findById(id);
+        return foundDeposit.orElseThrow(() ->
+                new DepositNotFoundException("Client with id " + id + " not found"));
+    }
 }
